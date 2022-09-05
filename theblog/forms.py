93a9af8823cd80da379971011687417e5de1a4da
dymasius <dymasius12@gmail.com>
2022-09-05 21:04:39 +0800

    Debugged the Category Field Error problem, basically there is a typo between models.py, admin.py, forms.py

--- theblog/forms.py
+++ theblog/forms.py
@@ -1,16 +1,20 @@
 # from socket import fromshare
 from django import forms
-from .models import Post
+from .models import Post, Category
+
+choices = [('coding', 'coding'), ('sports', 'sports'), ('entertainment', 'entertainment'),]
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'author', 'body')
+        fields = ('title', 'title_tag', 'author', 'category', 'body')
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
+            'category': forms.Select(choices=choices, attrs={'class': 'form-control'}),
+            # 'category': forms.Select(attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
         }
 
